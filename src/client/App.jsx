9b37eaa9caf3a344d698aeb7298555e9a9610964
David <czdavid93@gmail.com>
2022-06-16 16:13:57 +0200

    I think i completed the challange

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,30 +1,76 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import MovieForm from './components/MovieForm';
-import UserForm from './components/UserForm';
+import { useEffect, useState } from "react";
+import "./App.css";
+import MovieForm from "./components/MovieForm";
+import UserForm from "./components/UserForm";
 
-const apiUrl = 'http://localhost:4000';
+const apiUrl = "http://localhost:4000";
 
 function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     fetch(`${apiUrl}/movie`)
-      .then(res => res.json())
-      .then(res => setMovies(res.data));
+      .then((res) => res.json())
+      .then((res) => setMovies(res.data));
   }, []);
 
   const handleRegister = async ({ username, password }) => {
-    
+    const user = { username: username, password: password };
+    fetch("http://localhost:4000/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((userData) => {
+        console.log(
+          `SUCCESSFULLY REGISTRED WITH USERNAME: ${userData.data.username}`
+        );
+      });
   };
 
   const handleLogin = async ({ username, password }) => {
-    
+    const user = { username: username, password: password };
+    fetch("http://localhost:4000/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((userData) => {
+        localStorage.setItem("token", `${userData.data}`);
+        console.log(
+          "I LOGGED IN, GOT BACK A JWT FROM API, AND IT IS NOW SAVED TO LOCAL STORAGE."
+        );
+      });
   };
-  
+
   const handleCreateMovie = async ({ title, description, runtimeMins }) => {
-    
-  }
+    const movie = {
+      title: title,
+      description: description,
+      runtimeMins: runtimeMins,
+    };
+
+    const token = localStorage.getItem("token");
+
+    fetch("http://localhost:4000/movie", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(movie),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies([...movies, data.data]);
+      });
+  };
 
   return (
     <div className="App">
@@ -39,7 +85,7 @@ function App() {
 
       <h1>Movie list</h1>
       <ul>
-        {movies.map(movie => {
+        {movies.map((movie) => {
           return (
             <li key={movie.id}>
               <h3>{movie.title}</h3>
--- src/server/controllers/user.js
+++ src/server/controllers/user.js
@@ -1,39 +1,36 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-const jwtSecret = 'mysecret';
-
 const register = async (req, res) => {
-    const { username, password } = req.body;
-
-    const createdUser = null;
-
-    res.json({ data: createdUser });
+  const { username, password } = req.body;
+  const encryptedPassword = await bcrypt.hash(password, 10);
+  const createdUser = await prisma.user.create({
+    data: {
+      username: username,
+      password: encryptedPassword,
+    },
+  });
+  res.json({ data: createdUser });
 };
 
 const login = async (req, res) => {
-    const { username, password } = req.body;
-
-    const foundUser = null;
-
-    if (!foundUser) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
-
-    const passwordsMatch = false;
-
-    if (!passwordsMatch) {
-        return res.status(401).json({ error: 'Invalid username or password.' });
-    }
-
-    const token = null;
-
-    res.json({ data: token });
+  const { username, password } = req.body;
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      username: username,
+    },
+  });
+  const passwordsMatch = await bcrypt.compare(password, foundUser.password);
+  if (!foundUser || !passwordsMatch) {
+    return res.status(401).json({ error: "Invalid username or password." });
+  }
+  const token = jwt.sign({ username }, process.env.JWT_SECRET);
+  res.json({ data: token });
 };
 
 module.exports = {
-    register,
-    login
+  register,
+  login,
 };
